977ded32b1142c8a69a33aa62e70240b85460872
pliam <1033496103@qq.com>
2021-08-14 22:29:46 +0800

    pandas-test

--- test.py
+++ test.py
@@ -1,2 +1,17 @@
 import streamlit as st
+import pandas as pd
+import codecs
+from pandas_profiling import ProfileReport
+import streamlit.components.v1 as components
+from streamlit_pandas_profiling import st_profile_report
+
+
+
+st.subheader("Automated EDA with Pandas Profile")
+data_file = st.file_uploader("Upload CSV",type=['csv'])
+if data_file is not None:
+    df = pd.read_csv(data_file)
+    st.dataframe(df.head())
+    Profile = ProfileReport(df)
+	st_profile_report(Profile)
 
